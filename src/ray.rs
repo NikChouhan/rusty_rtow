07f630c6cc4a66c25b748422819241977234cc85
Nikhil Chouhan <nikhilchouhan@gmail.com>
2024-04-17 18:01:26 +0530

    commit ray struct

---
+++ src/ray.rs
@@ -0,0 +1,25 @@
+use super::vec::Vec3;
+use super::vec::Point3;
+
+struct Ray {
+    orig: Point3,
+    dir: Vec3
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Ray {
+        Ray {origin, direction}
+    }
+
+    pub fn direction(&self) -> Vec3 {
+        self.dir
+    }
+
+    pub fn origin(&self) -> Point3 {
+        self.orig
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        self.orig + t*self.dir
+    }
+}
