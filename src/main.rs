4a846f12cfcb009a2bc5e721550eb6c6a264046c
Nikhil Chouhan <nikhilchouhan@gmail.com>
2024-04-17 17:35:00 +0530

    main.rs changes to make it modular

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+mod vec;
 use std::{fs::File,io::{BufWriter, Write}};
+use vec::{Vec3, Color};
 
 fn main(){ 
-    const IMAGE_WIDTH : u32 = 256;
-    const IMAGE_HEIGHT : u32 = 256;
+    const IMAGE_WIDTH : u64 = 256;
+    const IMAGE_HEIGHT : u64 = 256;
 
     let file = File::create("image.ppm").expect("unable to create file");
     //Render
@@ -11,17 +13,13 @@ fn main(){
 
     write!(file,"P3\n{} {} \n255\n", IMAGE_WIDTH, IMAGE_HEIGHT).expect("unable to write");
 
-    for j in 0..IMAGE_WIDTH {
-        for i in 0..IMAGE_HEIGHT {
-            let r : f32 = (i as f32)/(IMAGE_WIDTH-1) as f32;
-            let g : f32 = (j as f32)/(IMAGE_HEIGHT-1) as f32;
-            let b: f32 = 0.0;
+    for j in 0..IMAGE_HEIGHT {
+        for i in 0..IMAGE_WIDTH {
+            let pixel_color = Color::new((i as f64) / ((IMAGE_WIDTH-1) as f64),
+                                               (j as f64) / ((IMAGE_HEIGHT-1) as f64),
+                                               0.25);
 
-            let ir : u32 = (255.99 * r) as u32;
-            let ig : u32 = (255.99 * g) as u32;
-            let ib : u32 = (255.99 * b) as u32; 
-
-        write!(file, "{} {} {} \n", ir, ig, ib).expect("Error in writing to the last line");
+        write!(file, "{} \n", pixel_color.format_color()).expect("Error in writing to the last line");
             
         }
     }  
