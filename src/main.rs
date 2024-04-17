730f07b8f7e7a60bb4646f0d5912cf8569bdc458
Nikhil Chouhan <nikhilchouhan@gmail.com>
2024-04-18 01:24:24 +0530

    sphere with normals visualised

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
 mod vec;
 mod ray;
 
-use std::{fs::File,io::{BufWriter, Write}};
+use std::{fs::File, io::{BufWriter, Write}};
 use vec::{Vec3, Point3, Color};
 use ray::Ray;
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
+    let oc = *center - r.origin();
+    let a = r.direction().dot(r.direction());
+    let b = -2.0 * r.direction().dot(oc);
+    let c = oc.dot(oc) - radius*radius;
+    let discriminant = b*b - 4.0*a*c;
+
+    if discriminant < 0.0 {
+        return -1.0;
+    }
+    else {
+        return (-b - f64::sqrt(discriminant)) / (2.0 *a);
+    }
+}
+
 fn ray_color(r: &Ray) -> Color {
+
+    let t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r);
+
+    if t>0.0 {
+        let n = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).normalised();
+
+        return 0.5*Color::new(n.x()+1.0, n.y()+1.0, n.z()+1.0);
+    }
+
     let t = 0.5 * (r.direction().normalised().y() +1.0);
     let color : Color = (t) * Color::new(1.0, 1.0, 1.0) + (1.0-t)*Color::new(0.5, 0.7, 1.0);
     return color;
 }
 
 fn main(){ 
-    const ASPECT_RATIO : f64 = 16.0 / 9.0;
+    const ASPECT_RATIO : f64 = 1.0;
     const IMAGE_WIDTH : u64 = 400;
     const IMAGE_HEIGHT : u64 = (IMAGE_WIDTH as f64/ASPECT_RATIO) as u64;
 
